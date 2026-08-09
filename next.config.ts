44ff4d95ddd64481fb0108d751da19c4aa6582e7
binivonditschi <binivonditschi@gmail.com>
2026-08-09 12:49:41 +0800

    next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+export default nextConfig;
