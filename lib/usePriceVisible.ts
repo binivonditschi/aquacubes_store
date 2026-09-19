71114cf3b082e3520db1067f23b2bde82992a820
binivonditschi <binivonditschi@gmail.com>
2026-09-20 04:55:21 +0800

    Update usePriceVisible.ts

--- lib/usePriceVisible.ts
+++ lib/usePriceVisible.ts
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 
-const ALLOWED_COUNTRIES = ["DE", "CH", "DK", "SE", "NO"];
+const ALLOWED_COUNTRIES = ["DE", "CH", "DK", "IT", "SE", "NO"];
 
 function getCookie(name: string): string | null {
   const match = document.cookie.match(new RegExp(`(?:^|; )${name}=([^;]*)`));
